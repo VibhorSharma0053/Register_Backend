3fe9668a45223b6a3d0adabff2354e3779aa43f1
VibhorSharma0053 <144813390+VibhorSharma0053@users.noreply.github.com>
2025-07-15 16:20:20 +0530

    run.py file for deployment

--- app/models.py
+++ app/models.py
@@ -1,9 +1,18 @@
 from pydantic import BaseModel
-from typing import Optional
+from typing import Optional, List
+
+class WorkEntry(BaseModel):
+    date: str
+    work: str
+    rate_per_unit: float
+    no_of_units: int
+    amount: float
+    deposit_or_due: Optional[float] = 0.0
 
 class Employee(BaseModel):
     name: str
     title: str = "New Designation"
     earned: float = 0.0
-    image: Optional[str] = None
     initials: Optional[str] = None
+    image: Optional[str] = None
+    work_entries: List[WorkEntry] = []  # New field added
